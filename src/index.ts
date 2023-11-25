caaf6d3fc48ed20293e778bf83bec7ddc0aa770c
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2023-11-25 17:08:28 +0000

    some minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-// import express from 'express'
+import express from 'express'
 
-// const app = express()
-// const port = 5000
-// app.get('/', (request, response) => {
-//     response.send('Hello world!')
-// })
-// app.listen(port, () => console.log(`Running on port ${port}`))
+const app = express()
+const port = 8080
 
-console.log("Hello world!")
+app.get('/', (request, response) => {
+    response.send('Hello world!')
+})
+
+app.listen(port, () => console.log(`Running on port ${port}`))
