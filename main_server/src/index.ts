57d8690ac4e8e0d7ae8b043c8a6ee34623c7c5b0
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2023-12-06 04:42:23 +0000

    Build report

--- main_server/src/index.ts
+++ main_server/src/index.ts
@@ -31,15 +31,36 @@ const formatApiResponse = (data: any) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const formatObject = (obj: any) => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { user_id, type, amount, ...rest } = obj;
+        const { user_id, type, amount, totalAmount, ...rest } = obj;
         return Object.fromEntries(
-            Object.entries({ ...rest, type: getTypeString(type), amount: amount / 100 })
+            Object.entries({ ...rest, type: getTypeString(type), amount: amount / 100, totalAmount: totalAmount / 100 })
                 .filter(([_, value]) => value !== null)
         );
     };
 
     return Array.isArray(data) ? data.map(formatObject) : formatObject(data);
 };
+
+const aggregateTransactionsByCategory = (transactions: Array<any>, transactionType: string) => {
+    return transactions.reduce((acc, transaction) => {
+        // Filter transactions by the specified type
+        if (getTypeString(transaction.type) !== transactionType) {
+            return acc;
+        }
+
+        // Handle null or undefined categoryId as 'Uncategorized'
+        const categoryId = transaction.categoryId || 'Uncategorized';
+
+        if (!acc[categoryId]) {
+            acc[categoryId] = { totalAmount: 0, count: 0};
+        }
+        acc[categoryId].totalAmount += transaction.amount / 100;
+        acc[categoryId].count += 1;
+
+        return acc;
+    }, {});
+};
+
 
 const getUserId = (auth: AuthResult | undefined) => {
     if (auth === undefined) {
@@ -393,6 +414,44 @@ reportRouter.delete('/:id', checkJwt, async (request, response) => {
         handleError(error, response);
     }
 });
+
+reportRouter.get('/build/:id', checkJwt, async (request, response) => {
+    try {
+        const userId = getUserId(request.auth);
+        const reportId = parseInt(request.params.id);
+
+        // Fetch the report details
+        const report = await prisma.report.findUnique({
+            where: {
+                id: reportId,
+                user_id: userId,
+            },
+        });
+
+        if (!report) {
+            return response.status(404).send('Report not found');
+        }
+
+        // Fetch transactions that match the report criteria
+        const transactions = await prisma.transaction.findMany({
+            where: {
+                user_id: userId,
+                date: {
+                    gte: report.periodStart,
+                    lte: report.periodEnd,
+                },
+            }
+        });
+
+        // Aggregate the transactions by category
+        const aggregatedData = aggregateTransactionsByCategory(transactions, getTypeString(report.type));
+        response.json(aggregatedData);
+    } catch (error) {
+        handleError(error, response);
+    }
+});
+
+
 
 app.use('/category', categoryRouter);
 app.use('/transaction', transactionRouter);
