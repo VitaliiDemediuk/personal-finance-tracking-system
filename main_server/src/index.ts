e5b44bb35ffc872806aef34d47ce896a74532928
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2023-12-06 00:52:19 +0000

    report request handlers

--- main_server/src/index.ts
+++ main_server/src/index.ts
@@ -279,7 +279,123 @@ transactionRouter.delete('/:id', checkJwt, async (request, response) => {
     }
 });
 
+//////////// Report Routes //////////////////////////////
+
+const reportRouter = express.Router();
+
+reportRouter.get('/all', checkJwt, async (request, response) => {
+    try {
+        const userId = getUserId(request.auth);
+
+        const reports = await prisma.report.findMany({
+            where: { user_id: userId }
+        });
+        response.json(formatApiResponse(reports));
+    } catch (error) {
+        handleError(error, response);
+    }
+});
+
+reportRouter.get('/:id', checkJwt, async (request, response) => {
+    try {
+        const userId = getUserId(request.auth);
+
+        const id = parseInt(request.params.id);
+        const report = await prisma.report.findUnique({
+            where: { id, user_id: userId },
+        });
+
+        if (report) {
+            response.json(formatApiResponse(report));
+        } else {
+            response.status(404).send('Report not found');
+        }
+    } catch (error) {
+        handleError(error, response);
+    }
+});
+
+reportRouter.post('', checkJwt, async (request, response) => {
+    try {
+        const userId = getUserId(request.auth);
+        const { name, periodStart, periodEnd, type } = request.body;
+
+        const convertedType = typesInt[type];
+        if (convertedType === undefined) {
+            return response.status(400).send('Invalid transaction type');
+        }
+
+        const startDate = new Date(periodStart);
+        const endDate = new Date(periodEnd);
+        if (startDate > endDate) {
+            return response.status(400).send('periodStart must be less than or equal to periodEnd');
+        }
+
+        const report = await prisma.report.create({
+            data: {
+                name,
+                createdOn: new Date(), // Automatically set the creation date
+                periodStart: startDate,
+                periodEnd: endDate,
+                type: convertedType,
+                user_id: userId,
+            },
+        });
+        response.json(formatApiResponse(report));
+    } catch (error) {
+        handleError(error, response);
+    }
+});
+
+reportRouter.put('/:id', checkJwt, async (request, response) => {
+    try {
+        const userId = getUserId(request.auth);
+
+        const id = parseInt(request.params.id);
+        const { name, periodStart, periodEnd, type } = request.body;
+
+        const convertedType = typesInt[type];
+        if (convertedType === undefined) {
+            return response.status(400).send('Invalid transaction type');
+        }
+
+        const startDate = new Date(periodStart);
+        const endDate = new Date(periodEnd);
+        if (startDate > endDate) {
+            return response.status(400).send('periodStart must be less than or equal to periodEnd');
+        }
+
+        const report = await prisma.report.update({
+            where: { id, user_id: userId },
+            data: {
+                name,
+                periodStart: startDate,
+                periodEnd: endDate,
+                type: convertedType,
+            },
+        });
+        response.json(formatApiResponse(report));
+    } catch (error) {
+        handleError(error, response);
+    }
+});
+
+reportRouter.delete('/:id', checkJwt, async (request, response) => {
+    try {
+        const userId = getUserId(request.auth);
+
+        const id = parseInt(request.params.id);
+        await prisma.report.delete({
+            where: { id: id, user_id: userId },
+        });
+        response.send();
+    } catch (error) {
+        handleError(error, response);
+    }
+});
+
 app.use('/category', categoryRouter);
 app.use('/transaction', transactionRouter);
+app.use('/report', reportRouter);
 
 app.listen(port, () => console.log(`Running on port ${port}`))
